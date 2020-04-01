1659db5a58de22c17a6d669658295799510b2529
Pete Wanca <pwanca@xsolis.com>
2020-04-01 17:28:23 -0500

    Updates

---
+++ precache-manifest.998f5cd00daabfef52f5ec8c4a3b1c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e52695cdccf89c0c287adcf4c507a31",
+    "url": "/akaisore/index.html"
+  },
+  {
+    "revision": "18ac21390e59ad7de226",
+    "url": "/akaisore/static/css/main.d8087f10.chunk.css"
+  },
+  {
+    "revision": "82c2ca47b398bf7f6682",
+    "url": "/akaisore/static/js/2.84bc88d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/akaisore/static/js/2.84bc88d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ac21390e59ad7de226",
+    "url": "/akaisore/static/js/main.eb7b547a.chunk.js"
+  },
+  {
+    "revision": "fbcbb8b5e1f2de93e791",
+    "url": "/akaisore/static/js/runtime-main.3c19c565.js"
+  }
+]);
